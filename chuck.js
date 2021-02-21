573ac24a937be0d485b3156c2bfce034d1dd45d0
Martin W <martin.wilhelmsson@utb.ecutbildning.se>
2021-02-21 20:32:19 +0100

    Added JS and very basic html for selecting categories with radiobuttons, and get a random chucknorris-joke.

--- chuck.js
+++ chuck.js
@@ -5,41 +5,105 @@
 'use strict';
 
 
-let getQuoteButton = document.getElementById("getQuoteButton");
+
+let getJokeButton = document.getElementById("getJokeButton");
+
 let quoteDisplay = document.getElementById("quoteDisplay");
+let categoriesDisplay = document.getElementById("categoriesDisplay");
+let selectedCategoryDisplay = document.getElementById("selectedCategoryDisplay");
+
+selectedCategoryDisplay.innerHTML = "<i>No category has been selected yet.</i>";
+let selectedCategory = ""; //used is API-call to chuck-site.
+
+const apiBase = 'https://api.chucknorris.io/';
+
+
+let setSelectedCategoryDisplay = function(category) {
+  if(!isString(category))
+  {
+    console.error(`${arguments.callee.name}() --> Input parameter 'category' was not of type 'string'.`);
+    selectedCategoryDisplay.innerHTML = "";
+    selectedCategory = "";
+    return;
+  }
+  selectedCategoryDisplay.innerHTML = "Selected category: "+category;
+  selectedCategory = category;
+}
+//... Because objects are passed as copy of reference: 
+//https://stackoverflow.com/questions/13104494/does-javascript-pass-by-reference
 
 let chuck = (function() {
-  //Vill bara ha det uppskrivet, samlat nånstans.
 
+  const xhttp = new XMLHttpRequest();
+  
   return {
-
-    
     infoVisible: false,
-    getQuote: function() {
 
-      var xhttp = new XMLHttpRequest();
+    getJoke: function() {
+      
+      let appendCategory = "";
+
+      //if a category is selected, then append it.
+      if(selectedCategory && typeof selectedCategory === 'string')
+        appendCategory = `?category=${selectedCategory}`
 
       xhttp.open("GET", 
-                  `https://api.chucknorris.io/jokes/random`, 
-                  true);
-      xhttp.send();
+              apiBase+`jokes/random`+appendCategory, 
+              true);
 
       xhttp.onreadystatechange = function () {
 
         if (xhttp.readyState==4 && xhttp.status==200) {
           let response = JSON.parse(xhttp.response);
-          console.log(response);
   
           if(response && response.value && typeof response.value === 'string')
             quoteDisplay.textContent = response.value;
           else
-            quoteDisplay.textContent = "Something went wrong when getting quote about Chuck norris.";
-        }
+            quoteDisplay.textContent = 'Something went wrong when getting quote about Chuck norris.';
+        } else 
+            quoteDisplay.textContent = 'Something went wrong fetching the Chuck Norris-FACT.';
         
       };
-    } 
+
+      xhttp.send();
+
+    },
+    getCategories: function() {
+
+      xhttp.open("GET", 
+                apiBase+`jokes/categories`, 
+                true);
+      
+      xhttp.onreadystatechange = function () {
+        if (xhttp.readyState==4 && xhttp.status==200) {
+          let response = JSON.parse(xhttp.response);
+  
+          if(response && typeof response === 'object')
+          {
+            (response).forEach(category => {
+              //build the radiobutton-menu and insert them.
+              let inputObject = createInputObject('radio', category, );
+              
+              inputObject.input.addEventListener('change', function(event) {
+                setSelectedCategoryDisplay(inputObject.label.innerHTML);
+              });
+              
+              categoriesDisplay.appendChild(inputObject.input);
+              categoriesDisplay.appendChild(inputObject.label);
+
+            });
+          }
+            
+          else
+            categoriesDisplay.textContent = 'Something went wrong when getting the categories about Chuck norris.';
+        } 
+      };
+
+      xhttp.send();
+    }
   }
 })();
 
+chuck.getCategories();
 
-getQuoteButton.onclick = chuck.getQuote;
+getJokeButton.onclick = chuck.getJoke;
---
+++ typeChecks.js
@@ -0,0 +1,7 @@
+//V0.0.1 2021-02-21
+
+let isString = function(val) {
+  if(val === null || typeof val !== 'string') 
+    return false;
+  return true;
+}
