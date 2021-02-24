dfbedfb6ace9e6a2bf0a824dbc793a072ae0b7a3
j-onas <jonas@hjulle.com>
2021-02-24 19:00:19 +0100

    Kommentarer

--- satellite.js
+++ satellite.js
@@ -3,23 +3,24 @@
 
 /*
 Todo: Fixa Kommentarer & rensa i koden
-
 */
+
 window.onload = (event) => {
     mapHandler();
     setInterval(mapHandler, 5000);
 }
 
+//Kalla på funktionen getCords och skicka de vidare till updateMap
 function mapHandler(){
     let cords = getCord().then(res => {
         let lat = res["lat"]
         let lon = res["lon"]
         updateMap(lon, lat);
-        console.log(lon, lat, "\n")
-        console.log("____________________")
     });
 }
 
+
+//Hämta koordinater
 async function getCord() {
     let lat = null;
     let lon = null;
@@ -34,6 +35,7 @@ async function getCord() {
     return {"lat":lat, "lon":lon}
 }
 
+//Skapar kartan & en lista med lager (layers)
 const map = new ol.Map({
     layers: [
         new ol.layer.Tile({
@@ -46,37 +48,40 @@ const map = new ol.Map({
 });
 
 function updateMap(lon, lat){   
+
+    //Ta bort gammla ikonen
     let lrs = map.getLayers().forEach(layer => {
         if(layer.get("name") == "icon"){
             map.removeLayer(layer)
         }
     })
     
+    //Skapa tomt objekt vid koordinaterna lon, lat
     let issFeature = new ol.Feature({
         geometry: new ol.geom.Point(ol.proj.fromLonLat([lon, lat]), ),
         name: "ISS"
     });
 
+    //Lägger till bilden, anchor 0.5 0.5 gör att bilden blir centrerad
     issFeature.setStyle(new ol.style.Style({
         image: new ol.style.Icon({
-            anchor: [0.5, 46],
-            anchorXUnits: 'fraction',
-            anchorYUnits: 'pixels',
+            anchor: [0.5, 0.5],
             src: "img/sat.png",
             scale: 0.1
         })
     }))
 
+    //Skapar en låda med våra features skapade ovan
     let iconLayerSource = new ol.source.Vector({
         features: [issFeature]
     })
 
+    //Skapar ett nytt layer att lägga till i map
     let iconLayer = new ol.layer.Vector({
         source: iconLayerSource,
         name: "icon"
     })
 
+    //Fäster ikonen på kartan 
     map.addLayer(iconLayer)
-    
-    
 }
